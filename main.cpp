71fdf6c1d1c5db8f1b9df254bbbed8ec1f4767df
rostislav <dadajanovrostislav@gmail.com>
2026-08-20 17:56:51 -0500

    configured osmium

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <cpr/cpr.h>
-#include <osmium::
+// 1. For reading the compressed .pbf file format
+#include <osmium/io/pbf_input.hpp>
+#include <osmium/handler.hpp>
+#include <osmium/visitor.hpp>
+
+#include <osmium/osm/node.hpp>
+#include <osmium/osm/way.hpp>
 
 int main(int argc, char* argv[]){
     std::cout << "Hello World!" << '\n';
